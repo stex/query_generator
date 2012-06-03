84af8b2d04272cee49bba14cf18ec50602cd2c79
Stefan Exner <stex@sterex.de>
2012-06-03 19:01:47 +0200

    abstraction

--- app/controllers/generated_queries_controller.rb
+++ app/controllers/generated_queries_controller.rb
@@ -1,7 +1,8 @@
 class GeneratedQueriesController < ApplicationController
 
+  layout "rails_query_generator"
+
   def index
-    QueryGenerator::DataHolder.exclusions = [Tolk::Locale, Tolk::Translation, Tolk::Phrase, Audit, Attachment, SheetLayout, VestalVersions::Version]
     @dh = QueryGenerator::DataHolder.instance
   end
 end
