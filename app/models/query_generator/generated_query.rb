04e85bd95c78ec8daddac20e4e22bf69dacdec27
Stefan Exner <stex@sterex.de>
2012-08-25 17:40:47 +0200

    finished first version of query generator

--- app/models/query_generator/generated_query.rb
+++ app/models/query_generator/generated_query.rb
@@ -21,28 +21,33 @@ module QueryGenerator
     def execute
       query = {}
       joins = joins_for(main_model_object)
-      query[:include] = joins if joins && joins.any?
+      query[:joins] = joins if joins && joins.any?
       query[:order] = order_by unless order_by.blank?
+      query[:group] = "#{main_model_object.table_name}.#{main_model_object.primary_key}"
 
       main_records = main_model_object.find(:all, query)
+
       rows = []
       main_records.each do |record|
         rows += build_rows_for(record)
       end
+
+      rows = rows.reject {|r| r.include?(nil)}
 
       rows
     end
 
     # If the given record or one of its associations has a has_many
     # association, we have to add additional rows to the result set
+    # TODO: find a better solution than arrays_index_merge. this will cost
+    #       too much time for large result sets
     #--------------------------------------------------------------
     def build_rows_for(record)
-      rows = [Array.new(output_columns.size)]
       model = record.class
-
       model_node = QueryGenerator::DataHolder.instance.linkage_graph.get_node(model)
+      rows = model_associations_for(model).any? ? [] : [Array.new(output_columns.size)]
 
-      model_associations[model].each do |association, target|
+      model_associations_for(model).each do |association, target|
         association_options = model_node.get_association_options(association)
 
         #a :1 association
@@ -60,14 +65,44 @@ module QueryGenerator
       rows.each do |row|
         columns.each_index do |index|
           if columns[index].model == model
-            row[index] = record.send(columns[index].column_name)
+            value = record.send(columns[index].column_name)
+            row[index] = value || ""
           end
         end
       end
 
-      rows
+      rows = merge_arrays_by_index(rows)
+      rows.uniq.compact
     end
 
+
+    def merge_arrays_by_index(arrays)
+      unless arrays.empty?
+        highest_index = arrays.first.size - 1
+
+        (0..highest_index).each do |index|
+          unique_value = unique_value_at?(arrays, index)
+          if unique_value
+            arrays.map {|a| a[index] = unique_value}
+          end
+        end
+      end
+
+      arrays
+    end
+
+    # Checks, if all given arrays have the same value or nil at the given position
+    # Returns the value if +true+, -false- otherwise
+    #--------------------------------------------------------------
+    def unique_value_at?(arrays, index)
+      value = nil
+      arrays.each do |array|
+        value ||= array[index]
+        return false if array[index] && array[index] != value
+      end
+
+      value
+    end
 
 
 
@@ -94,6 +129,10 @@ module QueryGenerator
       end
 
       @model_associations
+    end
+
+    def model_associations_for(model)
+      model_associations[model] || {}
     end
 
     # Returns all selected columns for the currently managed generated query
@@ -132,9 +171,9 @@ module QueryGenerator
         model_associations[model].each do |association, target|
           if is_end_association?(model, association)
             if association_amount == 1
-              return association
+              return association.to_sym
             else
-              joins << association
+              joins << association.to_sym
             end
           else
             if association_amount == 1
